eb810a854ea513aeccf2bd712eca03e2d9c261a3
carsonbennett <carben2004@gmail.com>
2025-01-27 13:34:51 -0800

    IncorporatingInputManager

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public Rigidbody sphereRigidbody;
-    public float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] float ballSpeed = 2f;
     void Start()
     {
         
+    }
+
+    public void MoveBall(Vector2 input)
+    {
+        Vector3 inputXYZPlane = new(input.x, 0, input.y);
+        sphereRigidbody.AddForce(inputXYZPlane * ballSpeed);
     }
 
     // Update is called once per frame
@@ -15,8 +21,8 @@ public class BallController : MonoBehaviour
         Vector2 inputVector = Vector2.zero;
         if (Input.GetKey(KeyCode.W)) inputVector += Vector2.up;
         if (Input.GetKey(KeyCode.A)) inputVector += Vector2.left;
-        if (Input.GetKey(KeyCode.S)) inputVector += Vector2.right;
-        if (Input.GetKey(KeyCode.D)) inputVector += Vector2.down;
+        if (Input.GetKey(KeyCode.S)) inputVector += Vector2.down;
+        if (Input.GetKey(KeyCode.D)) inputVector += Vector2.right;
 
         Debug.Log("Resultant Vector: " + inputVector);
 
