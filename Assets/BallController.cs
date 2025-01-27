5ebf6f3b7e808ae6d1011c0b2c99a4beaeb897d0
carsonbennett <carben2004@gmail.com>
2025-01-27 13:18:07 -0800

    AddingBallMovement

---
+++ Assets/BallController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody sphereRigidbody;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.W)) inputVector += Vector2.up;
+        if (Input.GetKey(KeyCode.A)) inputVector += Vector2.left;
+        if (Input.GetKey(KeyCode.S)) inputVector += Vector2.right;
+        if (Input.GetKey(KeyCode.D)) inputVector += Vector2.down;
+
+        Debug.Log("Resultant Vector: " + inputVector);
+
+    }
+}
